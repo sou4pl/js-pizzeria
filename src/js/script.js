4a63617459014ec80b4d46ccfea8bafaeb36cb61
Michał Słota <michal.k.slota@gmail.com>
2022-10-06 14:44:27 +0200

    add create dom element from html - not workin

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,40 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new product', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelectorAll(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp=this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +93,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
   app.init();
-}
+};

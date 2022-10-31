8d364e9913df167e3251b38783ec1a3000d4ad9d
Michał Słota <michal.k.slota@gmail.com>
2022-10-31 11:06:52 +0100

    add date datePicker and hourPicker

--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -1,5 +1,7 @@
 import {templates, select, } from '../settings.js';
 import AmountWidget from './amountWidget.js';
+import HourPicker from './HourPicker.js';
+import DatePicker from './DatePicker.js';
 
 class Booking {
   constructor(element){
@@ -17,6 +19,8 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
@@ -25,6 +29,8 @@ class Booking {
     //thisBooking.dom.peopleAmount.addEventListener('click', function(){console.log('poszło w ludzi!');});
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
     //thisBooking.dom.hoursAmount.addEventListener('click', function(){console.log('poszło w godziny');});
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
